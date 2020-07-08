b7dbc936246fd8888c90c37feee524d3803693a7
DannyDenver <djt09d@fulbrightmail.org>
2020-07-08 11:09:56 -0600

    initial commit

---
+++ src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
@@ -0,0 +1,57 @@
+package com.udacity.jwdnd.c1.review;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+public class ReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewApplication.class, args);
+	}
+
+	@Bean
+	public String message() {
+		return "Hello, Spring";
+	}
+
+	@Bean
+	public String uppercaseMessage(MessageService messageService) {
+		String upper = messageService.upperCase();
+		System.out.println(upper);
+		return upper;
+	}
+
+	@Bean
+	public String lowercaseMessage(MessageService messageService){
+		String lower = messageService.lowerCase();
+		System.out.println(lower);
+		return lower;
+	}
+
+
+	@Service
+	public class MessageService {
+		private String message;
+		public MessageService(String message) {
+			this.message = message;
+		}
+
+		public String upperCase() {
+			return message.toUpperCase();
+		}
+
+		public String lowerCase() {
+			return message.toLowerCase();
+		}
+	}
+}
