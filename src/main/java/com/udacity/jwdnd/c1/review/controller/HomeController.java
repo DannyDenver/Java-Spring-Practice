3cbeeea13ec5b118810a9fb1a5cd204b63b3708b
DannyDenver <djt09d@fulbrightmail.org>
2020-07-08 15:36:20 -0600

    add form for greetings

--- src/main/java/com/udacity/jwdnd/c1/review/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/c1/review/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.udacity.jwdnd.c1.review.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.Instant;
@@ -9,9 +12,27 @@ import java.time.Instant;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String getHomePage(Model model) {
-        model.addAttribute("greetings", new String[] { "yo", "howdy", "hallo", "bonjour"} );
+    private MessageListService messageListService;
+
+    public HomeController(MessageListService messageListService) {
+        this.messageListService = messageListService;
+    }
+
+    @GetMapping("/home")
+    public String getHomePage(@ModelAttribute("newMessage") MessageForm newMessage, Model model) {
+        model.addAttribute("greetings", this.messageListService.getMessages());
+
         return "home";
     }
+
+    @PostMapping("/home")
+    public String addMessage(@ModelAttribute("newMessage") MessageForm newMessage, Model model) {
+        messageListService.addMessage(newMessage.getText());
+        model.addAttribute("greetings", messageListService.getMessages());
+        newMessage.setText("");
+        return "home";
+    }
+
+
+
 }
